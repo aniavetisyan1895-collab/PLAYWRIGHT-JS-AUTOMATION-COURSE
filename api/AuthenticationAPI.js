07b3c387250dd9450299f0a39cda7dfe82efb4ff
Ani Avetisyan <aniavetisyan18.95@gmail.com>
2026-08-28 18:56:18 +0400

    Add API authentication

---
+++ api/AuthenticationAPI.js
@@ -0,0 +1,22 @@
+import { expect } from '@playwright/test';
+
+export class AuthenticationAPI {
+  constructor(request) {
+    this.request = request;
+  }
+
+  async apiLogin(email, password) {
+    const response = await this.request.post('/login', {
+      form: {
+        email,
+        password
+      }
+    });
+
+    expect(response.status()).toBe(200);
+
+    const cookies = await this.request.storageState();
+
+    return cookies.cookies;
+  }
+}
