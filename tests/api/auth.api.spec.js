2d9e819f1e1c42ff3333b8a28f2f89d0e803278d
Ani Avetisyan <aniavetisyan18.95@gmail.com>
2026-09-04 18:49:31 +0400

    Move signup test data to separate file

--- tests/api/auth.api.spec.js
+++ tests/api/auth.api.spec.js
@@ -1,28 +1,11 @@
 import { test, expect } from '@playwright/test';
 import { AuthenticationAPI } from '../../api/AuthenticationAPI';
+import { getSignupForm } from './testData';
 
 test('User can sign up successfully', async ({ request }) => {
   const authenticationAPI = new AuthenticationAPI(request);
 
-  const form = {
-    name: 'Ani',
-    email: `ani${Date.now()}@example.com`,
-    password: 'Test123!',
-    title: 'Mrs',
-    birth_date: '18',
-    birth_month: 'June',
-    birth_year: '1995',
-    firstname: 'Ani',
-    lastname: 'Avetisyan',
-    company: 'Test Company',
-    address1: 'Test Address',
-    address2: '',
-    country: 'Armenia',
-    zipcode: '00100',
-    state: 'Yerevan',
-    city: 'Yerevan',
-    mobile_number: '091234567'
-  };
+  const form = getSignupForm();
 
   const response = await authenticationAPI.apiSignup(form);
 
